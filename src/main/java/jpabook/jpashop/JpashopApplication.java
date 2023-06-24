f8545a3a7d6eaa7e92e6f1da6b2a076fe4ea93ca
JinSung-Hwang <wlstjd7782@gmail.com>
2023-06-24 12:40:18 +0900

    feature: 섹션7 웹 계층 개발 - 홈 화면과 레이아웃

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpashopApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
-
 }
 
 
